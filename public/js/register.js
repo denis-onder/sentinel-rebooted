ce5d5ddfdf9695b37ef9a43f139f53a69a7e3e7b
Denis Onder <denis_onder@protonmail.com>
2019-11-07 20:56:09 +0100

    Registration page scripts

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,27 @@
 const $ = id => document.getElementById(id).value || "";
 
+const showErrors = errObj => {
+  // Clear function
+  const clear = () => $("error_handler").remove();
+  // If the error handler is up, remove it first
+  if ($("error_handler")) clear();
+  // Generate error handler
+  const payload = `
+  <div id="error_handler">
+    <i class="fas fa-times error_handler_x"></i>
+    <div class="error_handler_output">
+      ${Object.keys(errMessages).map(
+        k => `<p class="error_handler_output_line">${errObj[k]}</p>`
+      )}
+    </div>
+  </div>
+`;
+  document.body.innerHTML += payload;
+  // Remove element after animation is done.
+  // The animation lasts for 5 seconds, hence the 5000ms timeout.
+  setTimeout(clear, 5000);
+};
+
 const register = async () => {
   const data = {
     firstName: $("first_name_input"),
@@ -10,6 +32,8 @@ const register = async () => {
   };
   const res = await fetch("/register", { method: "POST" }, data);
   const payload = await res.json();
-  if (res.status !== 200) console.error(payload);
+  if (res.status !== 200) showErrors(payload);
   console.log(`Registered`);
 };
+
+document.getElementById("submit_button").addEventListener("click", register);

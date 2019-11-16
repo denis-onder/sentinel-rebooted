53e1c68014cd494ce97fc1b2bad652a9182c977f
Denis Onder <denis_onder@protonmail.com>
2019-11-16 15:33:06 +0100

    Opening and closing the drawer

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,1 +1,9 @@
-console.log(window.location.pathname);
+(() => {
+  const btn = document.getElementById("dashboard_nav_menu_button");
+  const drawer = document.getElementById("drawer");
+  // Button listener
+  btn.onclick = () => {
+    drawer.classList.toggle("open");
+    btn.toggleAttribute("data-isopen");
+  };
+})();

85a9289a0f5069426fac665dff9083b8622b9c61
Denis Onder <denis_onder@protonmail.com>
2019-11-17 02:59:25 +0100

    Redirect to dashboard on landing if token is valid

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,5 +1,10 @@
 const elements = document.querySelectorAll(".landing_box_link_text");
 
+// If there's a auth cookie, redirect to the dashboard page.
+window.onload = () => {
+  if (document.cookie.includes("auth=")) window.location.href = "/dashboard";
+};
+
 // Hover event
 elements.forEach(elem => {
   // Add class
